0786a4b759ad6f2176d4b98354f265c768e23832
William Cheausky <williamchenausky@yahoo.com>
2021-04-26 13:40:21 -0400

    updated wrapper component

---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Wrapper.css"
+
+function Wrapper({children}) {
+    return (
+        <div className = "wrapper">
+            { children }
+        </div>
+    )
+}
+
+export default Wrapper
